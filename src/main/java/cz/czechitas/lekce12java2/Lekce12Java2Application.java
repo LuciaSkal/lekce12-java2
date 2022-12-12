b5e5453557ba5b3a5321c59887f5e724f6d05f14
LuciaSkal <skalinovalucia@gmail.com>
2022-12-12 18:53:52 +0100

    Initial commit

---
+++ src/main/java/cz/czechitas/lekce12java2/Lekce12Java2Application.java
@@ -0,0 +1,13 @@
+package cz.czechitas.lekce12java2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lekce12Java2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lekce12Java2Application.class, args);
+	}
+
+}
---
+++ src/test/java/cz/czechitas/lekce12java2/Lekce12Java2ApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.czechitas.lekce12java2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lekce12Java2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
